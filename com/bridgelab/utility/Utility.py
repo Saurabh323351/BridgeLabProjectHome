89d6527cb0db4dab757bd65dc769612750550266
Saurabh Singh <44753622+Saurabh323351@users.noreply.github.com>
2018-12-01 00:13:56 +0530

    Added two implementation of hashmap from home

--- com/bridgelab/utility/Utility.py
+++ com/bridgelab/utility/Utility.py
@@ -36,3 +36,156 @@ class Utility:
 
 utility_obj = Utility()
 
+
+-----------------hashing first implementation-------------------------
+
+class HashTable:
+    def __init__(self):
+        self.size = 11
+        self.slots = [None] * self.size
+        self.data = [None] * self.size
+
+    def put(self,key,data):
+      hashvalue = self.hashfunction(key,len(self.slots))
+
+      if self.slots[hashvalue] == None:
+        self.slots[hashvalue] = key
+        self.data[hashvalue] = data
+      else:
+        if self.slots[hashvalue] == key:
+          self.data[hashvalue] = data  #replace
+        else:
+          nextslot = self.rehash(hashvalue,len(self.slots))
+          while self.slots[nextslot] != None and \
+                          self.slots[nextslot] != key:
+            nextslot = self.rehash(nextslot,len(self.slots))
+
+          if self.slots[nextslot] == None:
+            self.slots[nextslot]=key
+            self.data[nextslot]=data
+          else:
+            self.data[nextslot] = data #replace
+
+    def hashfunction(self,key,size):
+         return key%size
+
+    def rehash(self,oldhash,size):
+        return (oldhash+1)%size
+
+    def get(self,key):
+      startslot = self.hashfunction(key,len(self.slots))
+
+      data = None
+      stop = False
+      found = False
+      position = startslot
+      while self.slots[position] != None and  \
+                           not found and not stop:
+         if self.slots[position] == key:
+           found = True
+           data = self.data[position]
+         else:
+           position=self.rehash(position,len(self.slots))
+           if position == startslot:
+               stop = True
+      return data
+
+    def __getitem__(self,key):
+        return self.get(key)
+
+    def __setitem__(self,key,data):
+        self.put(key,data)
+
+H=HashTable()
+H[54]="cat"
+H[26]="dog"
+H[93]="lion"
+H[17]="tiger"
+H[77]="bird"
+H[31]="cow"
+H[44]="goat"
+H[55]="pig"
+H[20]="chicken"
+print(H.slots)
+print(H.data)
+
+print(H[20])
+
+print(H[17])
+H[20]='duck'
+print(H[20])
+print(H[99])
+
+
+------------------hashmap 2nd implementatin----------------------------------------------
+
+
+
+# Hash Map
+
+class HashMap:
+	def __init__(self):
+		self.size = 6
+		self.map = [None] * self.size
+		
+	def _get_hash(self, key):
+		hash = 0
+		for char in str(key):
+			hash += ord(char)
+		return hash % self.size
+		
+	def add(self, key, value):
+		key_hash = self._get_hash(key)
+		key_value = [key, value]
+		
+		if self.map[key_hash] is None:
+			self.map[key_hash] = list([key_value])
+			return True
+		else:
+			for pair in self.map[key_hash]:
+				if pair[0] == key:
+					pair[1] = value
+					return True
+			self.map[key_hash].append(key_value)
+			return True
+			
+	def get(self, key):
+		key_hash = self._get_hash(key)
+		if self.map[key_hash] is not None:
+			for pair in self.map[key_hash]:
+				if pair[0] == key:
+					return pair[1]
+		return None
+			
+	def delete(self, key):
+		key_hash = self._get_hash(key)
+		
+		if self.map[key_hash] is None:
+			return False
+		for i in range (0, len(self.map[key_hash])):
+			if self.map[key_hash][i][0] == key:
+				self.map[key_hash].pop(i)
+				return True
+		return False
+			
+	def print(self):
+		print('---PHONEBOOK----')
+		for item in self.map:
+			if item is not None:
+				print(str(item))
+			
+h = HashMap()
+h.add('Bob', '567-8888')
+h.add('Ming', '293-6753')
+h.add('Ming', '333-8233')
+h.add('Ankit', '293-8625')
+h.add('Aditya', '852-6551')
+h.add('Alicia', '632-4123')
+h.add('Mike', '567-2188')
+h.add('Aditya', '777-8888')
+h.print()		
+h.delete('Bob')
+h.print()
+print('Ming: ' + h.get('Ming'))
+		
+
